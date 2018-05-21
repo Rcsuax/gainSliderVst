64e63b7f9589296ecd0aeb14718e637c3230b365
Rcsuax <rcsuax@gmail.com>
2018-05-21 13:17:01 +0100

    Init Project

---
+++ PluginEditor.cpp
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GainSliderAudioProcessorEditor::GainSliderAudioProcessorEditor (GainSliderAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+
+    setSize (400, 300);
+    
+    gainSlider.setSliderStyle(Slider::SliderStyle::LinearVertical);
+    gainSlider.setRange(-48.0, 0.0);
+    gainSlider.setValue(-1.0);
+    gainSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 100, 25);
+    gainSlider.addListener(this);
+    addAndMakeVisible(gainSlider);
+}
+
+GainSliderAudioProcessorEditor::~GainSliderAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void GainSliderAudioProcessorEditor::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+}
+
+void GainSliderAudioProcessorEditor::resized()
+{
+    gainSlider.setBounds(getLocalBounds());
+}
+
+void GainSliderAudioProcessorEditor::sliderValueChanged(Slider *slider) {
+    
+    // db = 20 * log(x)
+    // x = 10^(db/20)
+    // this algo allows for the human ear to percieve a linear change in volume
+    processor.rawVolume = pow(10, gainSlider.getValue() / 20);
+    
+    
+}
---
+++ PluginProcessor.cpp
@@ -0,0 +1,182 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GainSliderAudioProcessor::GainSliderAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+GainSliderAudioProcessor::~GainSliderAudioProcessor()
+{
+}
+
+//==============================================================================
+const String GainSliderAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool GainSliderAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool GainSliderAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool GainSliderAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double GainSliderAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int GainSliderAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int GainSliderAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void GainSliderAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String GainSliderAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void GainSliderAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void GainSliderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void GainSliderAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool GainSliderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void GainSliderAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        for (int sample = 0; sample < buffer.getNumSamples(); sample++) {
+            channelData[sample] = buffer.getSample(channel, sample) * rawVolume;
+        }
+    }
+}
+
+//==============================================================================
+bool GainSliderAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* GainSliderAudioProcessor::createEditor()
+{
+    return new GainSliderAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void GainSliderAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void GainSliderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new GainSliderAudioProcessor();
+}
